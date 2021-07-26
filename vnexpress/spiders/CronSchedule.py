90dcc9c8c74694f607b69a13d175063d83429500
nhamthinh <thinhnx@ominext.com>
2021-07-26 16:48:49 +0700

    add index.html

--- vnexpress/spiders/CronSchedule.py
+++
@@ -1,8 +0,0 @@
-from crontab import CronTab
-
-cron = CronTab(user='nhamthinh')
-job = cron.new(command='cd ~/projects/vnexpress && scrapy crawl home', comment='minute')
-job.hour.every(1)
-# job.minute.every(1)
-# cron.remove_all()
-cron.write()
